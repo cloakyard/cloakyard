034f661e22d5bca84bc5d27ab42bfacf2a3d8a84
Sumit Sahoo <sumitsahoo1988@gmail.com>
2026-08-14 12:37:41 +0530

    Unify the Cloakyard family design system

--- src/components/Brand.tsx
+++ src/components/Brand.tsx
@@ -1,10 +1,6 @@
-interface BrandProps {
-  inverse?: boolean;
-}
-
-export function Brand({ inverse = false }: BrandProps) {
+export function Brand() {
   return (
-    <a className={`brand${inverse ? " brand--inverse" : ""}`} href="/" aria-label="Cloakyard home">
+    <a className="brand" href="/" aria-label="Cloakyard home">
       <img className="brand-mark" src="/cloakyard-mark.svg" alt="" width="40" height="40" />
       <span className="brand-word" translate="no">
         Cloak<span>yard</span>
